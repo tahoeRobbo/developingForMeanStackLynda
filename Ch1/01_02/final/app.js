99d68a1e8b6a7735f30a6abc2e28437d2cbfd288
tahoeRobbo <rob@snowyplayground.com>
2015-07-06 23:28:57 -0600

    made separate file for me -~myApp~-

--- Ch1/01_02/final/app.js
+++ Ch1/01_02/final/app.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(process.env.IP, process.env.PORT);
+}).listen(1337, '127.0.0.1', console.log('listening on port ' + 1337));
